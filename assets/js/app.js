2e9d6ee93c609649be92f355bee704a9e274c11f
tarunmalik330 <maliktarun2005@gmail.com>
2023-09-21 12:10:59 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,79 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("end-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.getElementById("menubtn-icon").classList.toggle("cross")
+}
+setTimeout(() => {
+    console.log("Delayed for 5 second.");
+    document.getElementById("preloader").classList.add("opacity-0")
+    document.getElementById("preloader").classList.add("pointer_event_none")
+    document.body.classList.remove("overflow-hidden")
+}, 5000);
+$('.responsive').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 840,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+$('.custum-slider').slick({
+    dots: false,
+    arrows: false,
+    infinite: false,
+    speed: 5000,
+    autoplay: true,
+    autoplaySpeed: 0,
+    slidesToShow: 3,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: false
+            }
+        },
+        {
+            breakpoint: 840,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
